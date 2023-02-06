99cf86f0f529c981f6e3587b4f381412ffcb11c0
Caden Howell <cadenhowell0322@gmail.com>
2023-02-05 23:51:33 -0600

    Final update to main.py

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
+python3 -m venv env
+source env/bin/activate
 git clone https://github.com/cadenhowell/Ludobots.git
 cd Ludobots
 git checkout finalProject
+
 python3 -m pip install -r requirements.txt
 python3 search.py
