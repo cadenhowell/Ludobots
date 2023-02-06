c3821b3ea1b63b080f2de1fbeeb8fd9c8e76b820
Caden Howell <cadenhowell0322@gmail.com>
2023-02-05 23:33:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 os.system('git clone https://github.com/cadenhowell/Ludobots.git')
+os.system('cd Ludobots')
 os.system('git checkout finalProject')
 os.system('pip install -r requirements.txt')
 os.system('python3 search.py')
