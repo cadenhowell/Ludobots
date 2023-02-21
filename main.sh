f871f5b4313bdf70d2561317d6d7898e82b5797d
Caden Howell <cadenhowell0322@gmail.com>
2023-02-20 18:52:02 -0600

    Update script

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ python3 -m venv env
 source env/bin/activate
 git clone https://github.com/cadenhowell/Ludobots.git
 cd Ludobots
-git checkout finalProject
+git checkout 3DMorph
 
 python3 -m pip install -r requirements.txt
 python3 search.py
